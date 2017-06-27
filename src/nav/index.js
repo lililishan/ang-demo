efe371abf8b04bbb5eac3106f72f3feccf1708c2
lishan <lishan@addnewer.com>
2017-06-27 18:55:01 +0800

    nav

--- src/nav/index.js
+++ src/nav/index.js
@@ -1,4 +1,12 @@
 import navTemplate from './nav.jade'
 import navCtrl from './nav.controller'
+import './nav'
 export default angular.module('app.nav', [])
+    .component('appNav',{
+        template: navTemplate,
+        controller: navCtrl,
+        bindings: {
+            navData: '<'
+        }
+    })
     .name
