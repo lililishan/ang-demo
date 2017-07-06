8596f185c419d9256490b823c873c296852a34a9
lishan <lishan@addnewer.com>
2017-07-06 16:07:19 +0800

    index

--- src/list/index.js
+++ src/list/index.js
@@ -1,5 +1,6 @@
 import listTem from './list.jade'
 import listCtrl from './list.controller'
+import './list.styl'
 export default angular.module('app.list', [])
   .config(($stateProvider) => {
     'ngInject'
